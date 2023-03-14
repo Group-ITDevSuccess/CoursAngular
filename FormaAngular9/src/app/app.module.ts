d63db3958e6172c550e11fa50285e55b969ec6dc
ITDevSuccess <raharisontsidiany@gmail.com>
2023-03-14 08:33:04 +0300

    SAVE

--- FormaAngular9/src/app/app.module.ts
+++ FormaAngular9/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     NgModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
