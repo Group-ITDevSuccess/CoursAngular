37c0239ff6b6b08c36e4123a572ea892ae828646
ITDevSuccess <raharisontsidiany@gmail.com>
2023-03-14 09:45:28 +0300

    Fin pour le directive : NgIf et NgFor

--- FormaAngular9/src/app/directive/class/class.component.ts
+++ FormaAngular9/src/app/directive/class/class.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassComponent implements OnInit {
 
+  teams = [
+    'est',
+    'om',
+    'barca',
+    'milan'
+  ];
   est = true;
   milan= false;
   barca= false;
