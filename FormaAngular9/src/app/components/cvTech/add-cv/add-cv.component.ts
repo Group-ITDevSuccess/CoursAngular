7ac21410b323dd118aac21b5dd6f95fb11ba3f9c
ITDevSuccess <raharisontsidiany@gmail.com>
2023-03-16 09:03:22 +0300

    Notion de Formulaire de Template

--- FormaAngular9/src/app/components/cvTech/add-cv/add-cv.component.ts
+++ FormaAngular9/src/app/components/cvTech/add-cv/add-cv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-cv',
@@ -10,6 +11,10 @@ export class AddCvComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addPersonne(formulaire: NgForm){
+    console.log(formulaire);
   }
 
 }
